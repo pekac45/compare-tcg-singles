803585143f2c22feee9355f8d5e91094defcec5f
Michal Pekar <pekac45@gmail.com>
2019-01-04 22:43:02 +0000

    deployment

--- src/client/App.js
+++ src/client/App.js
@@ -14,6 +14,7 @@ const Footer = () => (
       <li>add bootstrap and make pretty</li>
       <li>add autosuggest to search</li>
       <li>implement champions</li>
+      <li>add real footer</li>
     </ul>
   </div>
 );
