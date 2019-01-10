f169fe54b62e2d33aad029fbc140ea8268cc50e1
Michal Pekar <pekac45@gmail.com>
2019-01-10 20:32:13 +0000

    radio buttons work and look pretty

--- src/client/Search.js
+++ src/client/Search.js
@@ -5,12 +5,10 @@ const Search = () => (
   <div>
     <form method="get">
       <div className="radioSelectors">
-        <label htmlFor="destiny" className="radioSelector destiny">
-          <input type="radio" name="game" value="destiny" />
-        </label>
-        <label htmlFor="champions" className="radioSelector champions">
-          <input type="radio" name="game" value="champions" />
-        </label>
+        <input id="destiny" type="radio" name="game" value="destiny" />
+        <label className="radioSelector destiny" htmlFor="destiny" />
+        <input id="champions" type="radio" name="game" value="champions" />
+        <label className="radioSelector champions" htmlFor="champions" />
       </div>
       <br />
       <input type="text" name="card" placeholder="Enter Card Name" />
