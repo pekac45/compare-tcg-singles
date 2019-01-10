3b7b28d654f6c0010befbd797f328c3005ab92d9
Michal Pekar <pekac45@gmail.com>
2019-01-10 23:13:40 +0000

    search bar and search button are pretty now

--- src/client/Footer.js
+++ src/client/Footer.js
@@ -10,15 +10,12 @@ const Footer = () => (
         </li>
         <li>change from table to list</li>
         <li>add links to results</li>
-        <li>
-          add bulma and make pretty
-          <ul>
-            <li>make radio buttons be pictures</li>
-            <li>add mobile hamburger menu</li>
-          </ul>
-        </li>
+        <li>add bulma and make pretty</li>
+        <li>make radio buttons be remembered with a cookie</li>
+        <li>add mobile hamburger menu</li>
         <li>add spinner + no results found</li>
         <li>add autosuggest to search</li>
+        <li>make logo, chose fonts</li>
         <li>add more shops</li>
         <li>implement caching</li>
         <li>add real footer</li>
--- src/client/Search.js
+++ src/client/Search.js
@@ -1,20 +1,68 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { Component } from 'react';
 import './app.css';
+import './search.css';
 
-const Search = () => (
-  <div>
-    <form method="get">
-      <div className="radioSelectors">
-        <input id="destiny" type="radio" name="game" value="destiny" />
-        <label className="radioSelector destiny" htmlFor="destiny" />
-        <input id="champions" type="radio" name="game" value="champions" />
-        <label className="radioSelector champions" htmlFor="champions" />
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = { selectedOption: 'destiny' };
+  }
+
+  handleOptionChange = changeEvent => {
+    this.setState({
+      selectedOption: changeEvent.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form method="get">
+          <div className="radioSelectors">
+            <input
+              id="destiny"
+              type="radio"
+              name="game"
+              value="destiny"
+              checked={this.state.selectedOption === 'destiny'}
+              onChange={this.handleOptionChange}
+            />
+            <label className="radioSelector destiny" htmlFor="destiny" />
+            <input
+              id="champions"
+              type="radio"
+              name="game"
+              value="champions"
+              checked={this.state.selectedOption === 'champions'}
+              onChange={this.handleOptionChange}
+            />
+            <label className="radioSelector champions" htmlFor="champions" />
+          </div>
+          <div className="columns is-centered searchBar">
+            <div className="column is-4">
+              <input
+                className="input"
+                type="text"
+                name="card"
+                placeholder="Enter Card Name"
+              />
+            </div>
+          </div>
+          <div className="columns is-centered searchSubmit">
+            <div className="column is-4">
+              <input
+                className="button is-fullwidth"
+                type="submit"
+                value="Search"
+              />
+            </div>
+          </div>
+        </form>
       </div>
-      <br />
-      <input type="text" name="card" placeholder="Enter Card Name" />
-      <input type="submit" value="Submit" />
-    </form>
-  </div>
-);
-
+    );
+  }
+}
 export default Search;
