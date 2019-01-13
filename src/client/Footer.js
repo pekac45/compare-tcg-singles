695e1c4e3464f483c96734d44203e55da96b336a
Michal Pekar <pekac45@gmail.com>
2019-01-13 23:57:39 +0000

    todo, refactor bulma in header

--- src/client/Footer.js
+++ src/client/Footer.js
@@ -2,16 +2,17 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    <strong>What&apos;s coming next?</strong>
+    <strong className="title is-3">What&apos;s coming next?</strong>
     <ul>
-      <li>make radio buttons be remembered with a cookie</li>
-      <li>add mobile hamburger menu</li>
-      <li>add autosuggest to search</li>
-      <li>make logo</li>
-      <li>add more shops</li>
-      <li>implement caching</li>
-      <li>add real footer</li>
-      <li>implement champions</li>
+      <li>Submitting will not refresh the page anymore.</li>
+      <li>Radio buttons will be remembered with a cookie.</li>
+      <li>Mobile hamburger menu.</li>
+      <li>Autosuggest to search.</li>
+      <li>Nicer logo and Home Button font.</li>
+      <li>More shops</li>
+      <li>Caching.</li>
+      <li>Nicer footer.</li>
+      <li>Warhammer Age of Sigmar: Champions.</li>
     </ul>
   </footer>
 );
--- src/client/Header.js
+++ src/client/Header.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bulma-components/full';
 
 const Header = () => (
-  <div>
-    <Navbar>
-      <navbar-brand>
-        <a className="navbar-item" href="http://localhost:3000/">
-          <img
-            src="../src/assets/favicon.ico"
-            width="28"
-            height="28"
-            alt="logo"
-          />
-          <h1>BLOBSHOP</h1>
-        </a>
-      </navbar-brand>
-    </Navbar>
+  <div className="Navbar">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="http://localhost:3000/">
+        <img
+          src="../src/assets/favicon.ico"
+          width="28"
+          height="28"
+          alt="logo"
+        />
+        <strong className="title is-3">BLOBSHOP</strong>
+      </a>
+    </div>
   </div>
 );
 
