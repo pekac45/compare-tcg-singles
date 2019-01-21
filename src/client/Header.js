c23e946e13341388b76278e09b03f114758efb27
Michal Pekar <pekac45@gmail.com>
2019-01-21 01:06:53 +0000

    logo

--- src/client/Header.js
+++ src/client/Header.js
@@ -3,13 +3,8 @@ import React from 'react';
 const Header = () => (
   <div className="Navbar">
     <div className="navbar-brand">
-      <a className="navbar-item" href="http://localhost:3000/">
-        <img
-          src="../src/assets/favicon.ico"
-          width="28"
-          height="28"
-          alt="logo"
-        />
+      <a className="navbar-item" href="http://blobshop.pekar.space">
+        <img src="../src/assets/logo.png" alt="logo" />
         <strong className="title is-3">BLOBSHOP</strong>
       </a>
     </div>
